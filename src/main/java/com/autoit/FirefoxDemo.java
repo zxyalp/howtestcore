1e234d528e83eb5f6fdb86d5c3300f60a8fc8ec6
醒z言 <orcexzy@163.com>
2017-06-02 17:53:34 +0800

    change

---
+++ src/main/java/com/autoit/FirefoxDemo.java
@@ -0,0 +1,23 @@
+package com.autoit;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+/**
+ * Created by yang.zhou on 2017/6/2.
+ */
+public class FirefoxDemo {
+    public static void main(String[] args) {
+
+//        System.setProperty("webdriver.firefox.marionette", "D:\\selenium\\geckodriver\\geckodriver.exe");
+//            System.setProperty("webdriver.gecko.driver", "D:\\selenium\\geckodriver\\geckodriver.exe");
+
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("http://www.baidu.com/");
+
+        WebElement loginBtn = driver.findElement(By.name("tj_login"));
+        loginBtn.click();
+    }
+}
