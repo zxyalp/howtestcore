b5d7802d94b7b30b69536689df73870bd440b67f
醒言 <orcexzy@163.com>
2018-04-22 15:52:04 +0800

    counter code

--- src/main/java/com/demo/POIDemo.java
+++ src/main/java/com/demo/POIDemo.java
@@ -1,12 +1,29 @@
 package com.demo;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileOutputStream;
+
 /**
  * @author yang.zhou
  * @date 2018/4/3
  */
 public class POIDemo {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception{
+
+        // xls
+        Workbook excel1997 = new HSSFWorkbook();
+        FileOutputStream fileOut = new FileOutputStream("demo1997.xls");
+        excel1997.write(fileOut);
+        excel1997.close();
+
+        // xlsx
+        Workbook excel2007 = new XSSFWorkbook();
+        FileOutputStream fileOut01 = new FileOutputStream("demo2007.xlsx");
+        excel2007.write(fileOut01);
+        excel2007.close();
     }
 }
